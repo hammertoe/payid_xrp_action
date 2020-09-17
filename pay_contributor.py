a6a31f9de23a0e9c54c22fd54517967857567631
Matt Hamilton <matthew.hamilton1@ibm.com>
2020-09-17 11:40:56 +0100

    initial version of pay_contributor, does nothing for now

---
+++ pay_contributor.py
@@ -0,0 +1,43 @@
+import xpring
+import requests
+import re
+import os
+
+def pay_xrp_testnet(wallet_seed, address, amount):
+    # Create a wallet instance using the seed
+    wallet = xpring.Wallet.from_seed(wallet_seed)
+
+    # The XRP testnet
+    url = 'test.xrp.xpring.io:50051'
+    client = xpring.Client.from_url(url)
+
+    # Create a transaction
+    txn = client.send(wallet, address, str(amount))
+
+    # Submit txn to the network
+    res = client.submit(txn)
+
+    return res
+
+def get_address_from_payid(payid, network, environment):
+    # Convert the PayID into a URL e.g.
+    # pay$username.github.io -> https://username.github.io/pay
+    local_part, domain = payid.split('$')
+    url = f'https://{domain}/{local_part}'
+    response = requests.get(url)
+    response.raise_for_status()
+
+    data = response.json()
+
+    # Look for an address that matches the network
+    # and environment we want to use
+    for address in data['addresses']:
+        if address['paymentNetwork'] == network and \
+           address['environment'] == environment:
+            return address['addressDetails']['address']
+
+def find_all_payids():
+    pass
+
+if __name__ == '__main__':
+    print("running pay contributor")
